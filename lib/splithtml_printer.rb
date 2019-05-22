c9ab61eae5e090888b29f378c653497f2f4286f9
rotemiara <39151776+rotemiara@users.noreply.github.com>
2019-05-22 11:46:44 +0300

    removed display filters

--- lib/splithtml_printer.rb
+++ lib/splithtml_printer.rb
@@ -205,16 +205,6 @@ private
     end
 
     REPORT_HEADER = <<-EOF
-  <div id="display-filters">
-    <input id="passed_checkbox"  name="passed_checkbox"  type="checkbox" checked="checked" onchange="apply_filters()" value="1" /> <label for="passed_checkbox">Passed</label>
-    <input id="failed_checkbox"  name="failed_checkbox"  type="checkbox" checked="checked" onchange="apply_filters()" value="2" /> <label for="failed_checkbox">Failed</label>
-    <input id="pending_checkbox" name="pending_checkbox" type="checkbox" checked="checked" onchange="apply_filters()" value="3" /> <label for="pending_checkbox">Pending</label>
-  </div>
-
-  <div id="summary">
-    <p id="totals">&#160;</p>
-    <p id="duration">&#160;</p>
-  </div>
 </div>
 
 
